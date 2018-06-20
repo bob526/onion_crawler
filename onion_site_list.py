ba96fbd0c3f8601ed15b6654028b972b264879ea
nari <invoid@csie.io>
2018-06-20 11:41:13 +0800

    use function to make it ease to call

--- onion_site_list.py
+++ onion_site_list.py
@@ -1,12 +1,22 @@
 import requests
 import json
 
-# Connect to Tor
-session = requests.session()
-session.proxies = {'http':  'socks5h://localhost:9050',
-                   'https': 'socks5h://localhost:9050'}
-
-respond = session.get('http://onionsnjajzkhm5g.onion/onions.php?format=json')
-
-fileptr = open('list.json', 'w')
-fileptr.write(respond.text)
+def writeOnionSiteList(listFileName):
+    '''
+    This function accesss the list of onion site provided by Daniel in json format.
+    listFileName should be a string containing the filename that you want to save the onion site list.
+    Recommend: from onion_site_list import *
+    '''
+    # Connect to Tor
+    print('Prepare to connect to Tor')
+    session = requests.session()
+    session.proxies = {'http':  'socks5h://localhost:9050',
+                       'https': 'socks5h://localhost:9050'}
+    print('Prepare to access onion site list')
+    respond = session.get('http://onionsnjajzkhm5g.onion/onions.php?format=json')
+    print('Access completed')
+    print('Open file: '+listFileName)
+    fileptr = open(listFileName, 'w')
+    fileptr.write(respond.text)
+    fileptr.close()
+    print('Close '+listFileName+'\nFunction complete')
