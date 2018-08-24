ea48c0057f15eac10c4bf4d15ed94f85b90bb804
nari <invoid@csie.io>
2018-08-24 13:49:12 +0800

    graduate commit

--- time_filter.py
+++ time_filter.py
@@ -1,20 +1,9 @@
 import datetime
 
-print(datetime.datetime.now())
-
-nowtime = datetime.datetime.now()
-oneday = datetime.timedelta(days=1)
-threeday = datetime.timedelta(days=3)
-yesterday = nowtime - oneday
-print(yesterday)
-
 # I want user pass an datetime objest representing the last uptime
 # And I compare the time with 'yesterday'
 # Return true = still alive, false = down site
 
-# Compare test 1
-print(yesterday < nowtime)  #If nowtime = uptime, it should be true = still alive
-print(yesterday < (nowtime-threeday)) # Should be false
 
 def stillUpOrNot(lastUpTime):
     '''
